ea0dd5537972882afd78fed6b9a6c76f2f40e5af
Daniil <dvinakovich@gmail.com>
2023-03-30 20:44:03 +0700

    Added widget

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+const widgetBtn = document?.querySelector('.widget')
+const widgetModal = document?.querySelector('.widget-modal')
+const widgetCloseBtn = document?.querySelector('.widget-modal__close')
+
+if (widgetBtn && widgetModal) {
+  const widgetClose = () => {
+    widgetModal.classList.remove('widget-modal--active')
+  }
+
+  const widgetOpen = () => {
+    widgetModal.classList.add('widget-modal--active')
+  }
+
+  widgetModal.addEventListener('click', event => {
+    if (event.target && event.target.classList.contains('widget-modal')) {
+      widgetClose()
+    }
+  })
+
+  widgetBtn.addEventListener('click', widgetOpen)
+  widgetCloseBtn.addEventListener('click', widgetClose)
+}
